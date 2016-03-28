7b5466169986fdc377ef61c60897e02280b8ff32
Joao Godinho <joaogodinho_4@hotmail.com>
2016-03-28 21:44:29 +0100

    insertion of login info and some tree functions

--- Meta2/structs.h
+++ Meta2/structs.h
@@ -3,7 +3,7 @@
 typedef struct Node_elem{
 	struct Node_elem *left;
 	struct Node_elem *right;
-	char * nodeType;
+	char * node_type;
 	char * value;
 } Node;
 
--- Meta2/treeFuncs.c
+++ Meta2/treeFuncs.c
@@ -1,3 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+Node * insert_node(char * node_type)
+{
+	Node * node = (Node *) malloc(sizeof(Node));
+	node->node_type = node_type;
+	node->right=NULL;
+	node->left=NULL;
+
+	return node;
+}
+
+Node * insert_term_node(char * node_type, char* value)
+{
+	Node * node = insert_node(node_type);
+	node->vale = value;
+	return node;
+}
+
+void insert_child(Node * father, Node * child)
+{
+
+	Node * temp = father->left;
+
+	if(temp==NULL)
+	{
+		father->left = child;
+	}
+	else
+	{
+		while(temp->right != NULL)
+		{
+			temp = temp->right;
+		}
+		temp->right = child;
+	}
+
+}
