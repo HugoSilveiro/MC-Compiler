8234f30ee0a8763abb6015893a8652912722727a
HugoSilveiro <hugofilipe95@hotmail.com>
2016-04-02 17:14:37 +0100

    Some changes

--- Meta2/structs.h
+++ Meta2/structs.h
@@ -3,47 +3,57 @@
 
 /* types for the nodes */
 typedef enum {
-	NODE_PROGRAM,
-	NODE_VAR_PART,
-	NODE_FUNC_PART,
-	NODE_VAR_DECL,
-	NODE_FUNC_DECL,
-	NODE_FUNC_DEF,
-	NODE_DECL,
-	NODE_FUNC_DEF_2,
-	NODE_FUNC_PARAMS,
-	NODE_PARAMS,
-	NODE_VAR_PARAMS,
-	NODE_ASSIGN,
-	NODE_IF_ELSE,
-	NODE_REPEAT,
-	NODE_STAT_LIST,
-	NODE_VAL_PARAM,
-	NODE_WHILE,
-	NODE_WRITE_LN,
-	NODE_ADD,		/* addition */
-	NODE_AND,
-	NODE_CALL,
-	NODE_DIV,		/* division with truncation; op = 'div';  result = integer-type; ISO 7185-page 54 */
-	NODE_EQ,
-	NODE_GEQ,
-	NODE_GT,
-	NODE_LEQ,
-	NODE_LT,
-	NODE_MINUS,		/* sign-inversion (minus sign) */
-	NODE_MOD,		/* modulo */
-	NODE_MUL,		/* multiplication */
-	NODE_NEQ,
-	NODE_NOT,
-	NODE_OR,
-	NODE_PLUS,		/* identity (plus sign) */
-	NODE_REAL_DIV,	/* normal division; op = '/'; result = real-type; ISO 7185-page 54 */
-	NODE_SUB,		/* subtraction */
-	NODE_ID,
-	NODE_INTLIT,
-	NODE_CHRLIT,
-	NODE_STRLIT,
-	NODE_NULL		/* used for temp nodes */
+	NODE_Program,
+	NODE_Declaration,
+	//Declaração de variáveis
+	NODE_ArrayDeclaration,
+	NODE_FuncDeclaration,
+	NODE_FuncDefinition,
+	NODE_ParamList,
+	NODE_FuncBody,
+	NODE_ParamDeclaration,
+	//Statement_List
+	NODE_StatList,
+	NODE_If,
+	NODE_For,
+	NODE_Return,
+
+	//Operadores
+	NODE_Or,
+	NODE_And,
+	NODE_Eq,
+	NODE_Ne,
+	NODE_Lt,
+	NODE_Gt,
+	NODE_Le,
+	NODE_Ge,
+	NODE_Add,
+	NODE_Sub,
+	NODE_Mul,
+	NODE_Div,
+	NODE_Mod,
+	NODE_Not,
+	NODE_Minus,
+	NODE_Plus,
+	NODE_Addr,
+	NODE_Deref,
+	NODE_Store,
+	NODE_Comma,
+	NODE_Call,
+
+	//Terminais
+	NODE_Int,
+	NODE_Char,
+	NODE_Pointer,
+	NODE_Void,
+	NODE_Id,
+	NODE_Intlit,
+	NODE_Chrlit,
+	NODE_Strlit,
+
+	//Especial
+	NODE_NULL		
+
 
 } Node_Type;
 typedef struct Node_elem{
