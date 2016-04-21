675cfc58b4a1d9611ea2e6d97b14c3f948df8431
HugoSilveiro <hugofilipe95@hotmail.com>
2016-04-21 15:30:19 +0100

    starting semantics

--- Meta3/semantics.c
+++ Meta3/semantics.c
@@ -3,19 +3,57 @@
 #include <string.h>
 
 #include "semantics.h"
+#include "symbolTable.h"
+
+extern Table * symbol_table;
+Table * current_trable;
 
 #define DEBUG 1
 
 int build_table(Node* tree)
-{/*
-	while(tree->child != NULL)
+{
+	Node* temp = tree->child;
+	while(temp != NULL)
 	{
-
+		check_node(temp);
 	}
-	*/
+	
 }
 /*
 funcDeclaration
 TypeSpec
 declaration
 */
+
+void check_node(Node* tree)
+{
+	Symbol *symbol;
+	char name[100], type[100];
+	int param;
+	if(strcmp("ArrayDeclaration"), NODE_NAME[tree->node_type])
+	{
+		name = get_name(tree);
+		type = get_type(tree);
+		param = 0;
+		insert_symbol(symbol_table, symbol);
+	}
+}
+
+char * get_name(Node* tree)
+{
+	while(tree->child != NULL){
+		if(strcmp(NODE_NAME[tree->child], "Id") == 0){
+			return tree->child->value;
+		}
+	}
+}
+char * get_type(Node* tree)
+{
+	char aux[10];
+	while(tree->child != NULL){
+		if(strcmp(NODE_NAME[tree->child], "IntLit") == 0){
+			aux = tree->child->value;
+		}
+		else if()
+	}
+}
