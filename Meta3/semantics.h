3a6cc9f50a54b83a015bc513e5511e9429df2df7
Joao Godinho <joaogodinho_4@hotmail.com>
2016-04-21 16:30:30 +0100

    oskdoskaoks

--- Meta3/semantics.h
+++ Meta3/semantics.h
@@ -2,3 +2,7 @@
 #include "structs.h"
 
 int build_table(Node* tree);
+void check_node(Node* tree);
+char * get_name(Node* tree);
+char * get_type(Node* tree);
+void insert_function_declaration(Node * tree);
