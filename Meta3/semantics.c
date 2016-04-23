e72d3ea2ac4f8203b50e2d64042aa9b3a9afb031
Joao Godinho <joaogodinho_4@hotmail.com>
2016-04-23 12:11:48 +0100

    asterisk working

--- Meta3/semantics.c
+++ Meta3/semantics.c
@@ -241,6 +241,7 @@ char * get_param_decl(Node * node)
 	Node * temp;
 	Symbol * symbol = NULL;
 	temp = node->child;
+
 	while(temp!=NULL)
 	{
 
@@ -258,9 +259,14 @@ char * get_param_decl(Node * node)
 		}
 		else if(strcmp(NODE_NAME[temp->node_type], "Int") == 0)
 		{
-			type = (char*)malloc(sizeof(char)*3);
+			type = (char*)malloc(sizeof(char)*4);
 			//memset(value, '\0', sizeof("int"));
 			strcpy(type, "int");
+		}
+		else if(strcmp(NODE_NAME[temp->node_type], "Pointer") == 0)
+		{
+			strcat(type, "*");
+		
 		}
 		//falta tratar o asterisco
 
@@ -319,11 +325,15 @@ void get_param_declaration(Node * node, Table *function)
 		}
 		else if(strcmp(NODE_NAME[temp->node_type], "Int") == 0)
 		{
-			type = (char*)malloc(sizeof(char)*3);
+			type = (char*)malloc(sizeof(char)*4);
 			//memset(value, '\0', sizeof("int"));
 			strcpy(type, "int");
 		}
 		//falta tratar o asterisco
+		else if(strcmp(NODE_NAME[temp->node_type], "Pointer")==0)
+		{
+			strcat(type, "*");
+		}
 		else
 		{
 			id = (char*)malloc(sizeof(temp->value));
@@ -373,6 +383,10 @@ char * get_function_typespec(Node * node)
 		{
 			//memset(value, '\0', sizeof("int"));
 			strcpy(value, "int");
+		}
+		else if(strcmp(NODE_NAME[temp->node_type], "Pointer")== 0)
+		{
+			strcat(value, "*");
 		}
 		else
 		{
