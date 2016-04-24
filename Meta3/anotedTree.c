f55963914cada9f28f04dd23be4a8a07c65535d6
HugoSilveiro <hugofilipe95@hotmail.com>
2016-04-24 15:36:55 +0100

    changes in insert_funcion_definition

--- Meta3/anotedTree.c
+++ Meta3/anotedTree.c
@@ -42,63 +42,7 @@ void insert_function_definition(tree)
 		current_table = NULL;
 
 	}
-	else
-	{
-		////////
-		printf("[Insertion on function definition]\n");
-		
-		Symbol * symbol;
-
-		//inserir o symbolo na tabela global
-		Table * global = search_table("global");
-
-
-		//inserir o typespec concatenado com os parametros...
-
-		//variavel para guardar o tipo de retorno da função
-		char *func_type;
-		func_type = strdup(get_function_typespec(node));
-
-		//variavel para guardar a lista de parametros no formato (%s,...,)
-		char *param_lists;
-		param_lists = strdup(get_param_list_concatenated_function(node));
-
-		//variavel para guardar os parametros no formato return(%s,...,);
-		char *params_concat;
-		params_concat = (char*) malloc(sizeof(func_type)+sizeof(param_lists));
-		sprintf(params_concat,"%s%s" , func_type, param_lists);
-
-		printf("Type: %s | param_lists: %s\n", func_type, param_lists);
-
-
-		//criação do simbolo para a tabela global
-		symbol = create_symbol(func_name, params_concat, 0);
-		insert_symbol(global, symbol);
-
-		//criar uma tabela nova
-		Table * aux1;
-
-
-		//inserir essa tabela à tabela de simbolos
-		aux1 = insert_table(1, func_name);
-		aux1->defined = 1;
-
-
-
-		//inserir o simbolo de return
-		symbol = create_symbol("return", func_type, 0);
-		insert_symbol(aux1, symbol);
-
-
-		//inserir os simbolos da lista de parametros
-		//inserir os parametros como symbolos
-		get_param_list_function(node, aux1);
-
-		current_table = aux1;
-		insert_function_funcBody(node);
-		
-		current_table = NULL;
-	}
+	
 }
 
 void insert_function_funcBody(Node * node)
