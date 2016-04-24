b66cf1df6b58eceeec39f8fd804eb1244994bc1d
HugoSilveiro <hugofilipe95@hotmail.com>
2016-04-24 16:12:04 +0100

    runnable

--- Meta3/anotedTree.h
+++ Meta3/anotedTree.h
@@ -8,3 +8,4 @@ void insert_funct_definition(Node * tree);
 void insert_funcBody(Node * node);
 void get_inside_funcBody(Node * node);
 void get_inside_id(Node * node);
+void get_inside_operator(Node * node);
