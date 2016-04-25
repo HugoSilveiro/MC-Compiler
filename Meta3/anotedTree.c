f004e81e80bbafef01471b5bd6316eb881e1e440
j-godinho <joaogodinho_4@hotmail.com>
2016-04-25 16:27:46 +0100

    call almost done

--- Meta3/anotedTree.c
+++ Meta3/anotedTree.c
@@ -6,7 +6,7 @@
 #include "anotedTree.h"
 #include "printer.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
 extern Table * symbol_table;
 Table * current_table2 = NULL;
@@ -95,15 +95,15 @@ void check_inside_funcBody(Node * node)
 		else if(strcmp(NODE_NAME[temp->node_type], "Store") == 0){
 			//Ver o tipo dos filhos
 			temp->type = "int";
-			//get_inside_operator(temp);
-			get_inside_funcBody(temp);
+			get_inside_operator(temp);
+			//get_inside_funcBody(temp);
 
 		}
 		else if(strcmp(NODE_NAME[temp->node_type], "Call") == 0)
 		{
 			printf("call\n");
 			temp->type = check_call_type(temp); 
-			get_inside_operator(temp);
+			get_inside_funcBody(temp);
 
 		}
 
